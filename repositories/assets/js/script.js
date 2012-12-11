191c3384aabd21191304d7f874ae895c4bfe1c22
roine <feunetre@hotmail.com>
2012-12-11 16:36:15 +0800

    merge gh-pages

--- repositories/assets/js/script.js
+++ repositories/assets/js/script.js
@@ -1,4 +1,4 @@
-$.getJSON('https://api.github.com/users/roine/repos?sort=updated&?callback=?', function (response) {
+$.getJSON('https://api.github.com/users/roine/repos?sort=updated&callback=?', function (response) {
 	var repos = response.data;
 
 	var totalRepos = 0,
@@ -6,15 +6,23 @@ $.getJSON('https://api.github.com/users/roine/repos?sort=updated&?callback=?', f
 		listRepos = [];
 
 	$.each(repos, function(index, value){
+		
 		if(!repos[index].fork){
 			listRepos[i] = [{
-				'name': repos[index].name
-			}]
+				'name': repos[index].name,
+				'updated_at':repos[index].updated_at,
+				'created_at':repos[index].created_at
+			}];
 
-			i++;
+			
+
+
+			totalRepos = ++i;
+
 		}
 	});
 	$('.totalRepos').find('span').text(totalRepos);
-	console.log(listRepos)
+	
 });
+console.log(listRepos);
 console.log(jQuery.ajaxSetup('cache'))
