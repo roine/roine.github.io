d35c8494e2f15a9222e7135ce46da5b6a58e6421
roine <feunetre@hotmail.com>
2012-12-31 11:47:06 +0800

    added all the colors

--- repositories/assets/js/script.js
+++ repositories/assets/js/script.js
@@ -25,18 +25,8 @@
 		customUser = window.location.hash.substring(1),
 		user = customUser || 'roine',
 		lastRepoOverviewMax = 2,
-		languagesColor = {
-			'javascript':'#F15501',
-			'php':'#6E03C1',
-			'ruby':'#701516',
-			'scala':'#7DD3B0',
-			'java':'#B07219',
-			'c':'#555',
-			'shell':'#5861CE',
-			'python':'#3581BA',
-			'objective-c':'#438EFF'
-		};
-
+		// https://raw.github.com/doda/github-language-colors/master/colors.json
+		languagesColor = {"Arduino": "#bd79d1", "Java": "#b07219", "VHDL": "#543978", "Scala": "#7dd3b0", "Emacs Lisp": "#c065db", "Delphi": "#b0ce4e", "Ada": "#02f88c", "VimL": "#199c4b", "Perl": "#0298c3", "Lua": "#fa1fa1", "Rebol": "#358a5b", "Verilog": "#848bf3", "Factor": "#636746", "Ioke": "#078193", "R": "#198ce7", "Erlang": "#949e0e", "Nu": "#c9df40", "AutoHotkey": "#6594b9", "Clojure": "#db5855", "Shell": "#5861ce", "Assembly": "#a67219", "Parrot": "#f3ca0a", "C#": "#555", "Turing": "#45f715", "AppleScript": "#3581ba", "Eiffel": "#946d57", "Common Lisp": "#3fb68b", "Dart": "#cccccc", "SuperCollider": "#46390b", "CoffeeScript": "#244776", "XQuery": "#2700e2", "Haskell": "#29b544", "Racket": "#ae17ff", "Elixir": "#6e4a7e", "HaXe": "#346d51", "Ruby": "#701516", "Self": "#0579aa", "Fantom": "#dbded5", "Groovy": "#e69f56", "C": "#555", "JavaScript": "#f15501", "D": "#fcd46d", "ooc": "#b0b77e", "C++": "#f34b7d", "Dylan": "#3ebc27", "Nimrod": "#37775b", "Standard ML": "#dc566d", "Objective-C": "#f15501", "Nemerle": "#0d3c6e", "Mirah": "#c7a938", "Boo": "#d4bec1", "Objective-J": "#ff0c5a", "Rust": "#dea584", "Prolog": "#74283c", "Ecl": "#8a1267", "Gosu": "#82937f", "FORTRAN": "#4d41b1", "ColdFusion": "#ed2cd6", "OCaml": "#3be133", "Fancy": "#7b9db4", "Pure Data": "#f15501", "Python": "#3581ba", "Tcl": "#e4cc98", "Arc": "#ca2afe", "Puppet": "#cc5555", "Io": "#a9188d", "Max": "#ce279c", "Go": "#8d04eb", "ASP": "#6a40fd", "Visual Basic": "#945db7", "PHP": "#6e03c1", "Scheme": "#1e4aec", "Vala": "#3581ba", "Smalltalk": "#596706", "Matlab": "#bb92ac"};
 
 	/***********
 	 *
@@ -137,10 +127,10 @@
 	function createRepoBox (repo) {
 		var homepage = repo.homepage || repo.html_url,
 
-			ribbon = $(document.createElement('div'))
-			.addClass('ribbon')
+			language = $(document.createElement('div'))
+			.addClass('language')
 			.text(repo.language)
-			.css('background-color', languagesColor[repo.language.toLowerCase()]),
+			.css('background-color', languagesColor[repo.language]),
 
 			leftMenu = $(document.createElement('div'))
 			.addClass('details'),
@@ -175,7 +165,7 @@
 			})
 			.addClass('repo drop-shadow')
 			.append(leftMenu)
-			.append(ribbon)
+			.append(language)
 			.appendTo('.listRepos');
 
 		
@@ -228,7 +218,7 @@
 				.addClass(lang)
 				.text(reposCount)
 				.animate({'width': width+'%'}, 'slow')
-				.css('background-color', languagesColor[lang.toLowerCase()])
+				.css('background-color', languagesColor[lang])
 				.attr({'data-perc': Math.floor(width), 'data-repos':reposCount})
 				.appendTo(container);
 			}
@@ -337,7 +327,7 @@
 
 						// add some effect to each repo while hovering
 						$repo.hover(function(){
-							$(this).css('border-bottom', '10px solid '+languagesColor[repo.language.toLowerCase()]);
+							$(this).css('border-bottom', '10px solid '+languagesColor[repo.language]);
 						}, function(){
 							$(this).css('border-bottom', '');
 						});
